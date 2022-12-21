38d5a8b0b0dbe79cbd02e3177d03515a04b15e6f
Matias Amariya <me.em@outlook.com>
2022-12-21 14:24:34 -0300

    added node.js lite-server

---
+++ js/index.js
@@ -0,0 +1,11 @@
+import {Router} from './routes.js'
+const router = new Router()
+router.add('/', 'pages/home.html')
+router.add('/about', 'pages/about.html')
+router.add('/contato', 'pages/contato.html')
+router.add(404, 'pages/404.html')
+
+router.handle();
+
+window.onpopstate = () => router.handle();
+window.route = () => router.route()
